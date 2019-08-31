2a13c74765612d8ee413e1b7fa0363efc8d7cca8
Saber Nemati <m.saber.n@gmail.com>
2019-08-31 14:49:10 +0430

    Calculations with the first guess

--- hirth-joint.py
+++ hirth-joint.py
@@ -8,10 +8,10 @@ import numpy as np
 
 ##### Gear geometry #####
 # Inner radius
-Ri=25.0           # [mm]
+Ri=17.0           # [mm]
 d=2*Ri
 # Outer radius
-Ro=50.0           # [mm]
+Ro=24.75           # [mm]
 D=2*Ro
 # Mean radius
 Rm=(Ri+Ro)/2        # [mm]
@@ -20,16 +20,16 @@ Rm=(Ri+Ro)/2        # [mm]
 z=24
 
 # Smallest tooth height
-hp=4.5         # [mm]
+hp=2.78         # [mm]
 # Biggest tooth height
-hpp=9          # [mm]
+hpp=4.59          # [mm]
 # G Point (Center of Gravity)
 hG= (hp+hpp)/2/2        # [mm]
 
 # Smallest tooth thickness
-ap=5.41         # [mm]
+ap=3.92         # [mm]
 # Biggest tooth thickness
-app=10.82       # [mm]
+app=5.95       # [mm]
 
 # Tooth length
 L=Ro-Ri         # [mm]
@@ -43,7 +43,7 @@ nb=0
 r=0.3           # [mm]
 
 # Crown clearance
-s=0.3           # [mm]
+s=0.43           # [mm]
 
 # Load bearing percentage (0.65 for milled; 0.75 for grinded teeth)
 eta_z=0.75
@@ -58,7 +58,6 @@ T= 100e3          # [N-mm]
 
 # Total tangential force
 Fu=np.round(T/Rm,1)         # [N]
-
 print("Fu = " + str(Fu))
 
 
@@ -85,12 +84,19 @@ SF=2.5
 
 # Prelaod
 Fva=Fa*SF           # [N]
+print("Preload = " + str(Fva))
 
 # Maximum pressure on tooth flank
 p_max=np.round((Fva+Fa)/Az,1)
 print("p_max = " + str(p_max))
 
 
+# Factor c for 24 teeth
+c=0.114     
+
+# h estimation
+h=c*D-(2*r+s)
+print("h = " + str(h))
 
 
 
